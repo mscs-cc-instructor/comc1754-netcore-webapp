7b78e3b720d532780d36ae2ca1d3e50ee9973d95
Mark Swanson <mswanson@debian1754.comc1754.org>
2019-04-19 15:23:28 -0500

    Completed ex2b

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace comc1754_netcore_webapp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>();
     }
 }
